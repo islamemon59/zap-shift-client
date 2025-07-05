5acd88205fb102c15294e7abbeb62743c428b495
islamemon59 <islamemon59@gmail.com>
2025-07-05 16:11:31 +0600

    base url deploy in vercel

--- src/Hooks/AxiosSecure/UseAxios.jsx
+++ src/Hooks/AxiosSecure/UseAxios.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React from 'react';
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000/"
+    baseURL: "https://zap-shift-server-eight.vercel.app/"
 })
 
 const UseAxios = () => {
